828dfc65ea9965b2f3c4c5738ea74ac8a65ef800
brandon-gallas <brandon.gallas@fda.hhs.gov>
2024-10-30 16:53:49 -0400

    Add code to check if stage is already homed.

--- src/stages/Thorlabs/stage_set_origin_thorlabs_script.m
+++ src/stages/Thorlabs/stage_set_origin_thorlabs_script.m
@@ -1,13 +1,26 @@
 
 % stage_set_origin_thorlabs_script opens a connection to a thorlabs stage.
 %   This script is meant to be run in the base environment.
-disp("Is savedStage busy?")
-disp(savedStage.device.IsDeviceBusy)
+% Wait for device to be ready
+elapsed = 0;
+while savedStage.device.IsDeviceBusy
+    pause(0.25)
+    elapsed = elapsed + .25;
 
-fprintf("Homing x channel ...\n")
-savedStage.xChannel.Home(savedStage.timeout);
-fprintf("Homed\n")
+    if elapsed > savedStage.timeout
+        error('Stage is busy longer than timeout.');
+    end
+end
 
-fprintf("Homing y channel ...\n")
-savedStage.yChannel.Home(savedStage.timeout);
-fprintf("Homed\n")
+% If stage is not homed, home it!
+if(~savedStage.xChannel.Status.IsHomed)
+    fprintf("Homing xChannel ...\n")
+    savedStage.xChannel.Home(savedStage.timeout);
+end
+fprintf("xChannel is homed.\n")
+
+if(~savedStage.yChannel.Status.IsHomed)
+    fprintf("Homing yChannel ...\n")
+    savedStage.yChannel.Home(savedStage.timeout);
+end
+fprintf("yChannel is homed.\n")
